7227ff303c6abeb08f9270f2a84d407be4bfbea3
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-06-10 15:18:28 +0100

    Created a basic search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Dictionary from "./Dictionary";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer> Coded by Komal Ahmed</footer>
+      </div>
     </div>
   );
 }
-
 export default App;
