2d038d29cd0312043dee615f12480ec748de1962
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-06-11 12:06:19 +0100

    Added phonetics to the dictionary app

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,6 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <br />
       <strong>Definition:</strong> {props.meaning.definition}
       <br />
       <strong>Example:</strong> <em>{props.meaning.example}</em>
--- src/Results.js
+++ src/Results.js
@@ -5,8 +5,10 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        {" "}
-        <h2> {props.results.word}</h2>
+        <h2>{props.results.word}</h2>
+        <div>
+          <strong>Phonetic:</strong> {props.results.phonetic}
+        </div>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
